ca9f34a2703f1df53d9890b31683c38f61134f7c
Brandon Garling <bjg96@nau.edu>
2016-09-22 08:12:35 -0700

    Actually use the raycaster in main

--- source/main.c
+++ source/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include "json/json.h"
+#include "raycaster/raycaster.h"
 
 int main() {
 	char* inputFname = "resources/input.json";
     printf("Info: Reading input scene file '%s'...\n", inputFname);
 
 	JSONValue JSONRoot;
-	return read_json(inputFname, &JSONRoot);
+	if (read_json(inputFname, &JSONRoot) != 0)
+		return 1;
+
+	if (raycast(JSONRoot.data.dataArray, 100, 100) != 0)
+		return 1;
+
+	return 0;
 }
