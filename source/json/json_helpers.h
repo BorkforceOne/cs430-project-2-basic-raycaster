395079fb5a29f630020d850b4dc8074f0e804278
Brandon Garling <bjg96@nau.edu>
2016-09-21 22:56:12 -0700

    Don't import .h files in other .h files, instead give a prototype

--- source/json/json_helpers.h
+++ source/json/json_helpers.h
@@ -5,7 +5,10 @@
 #ifndef CS430_PROJECT_2_BASIC_RAYCASTER_JSON_HELPERS_H
 #define CS430_PROJECT_2_BASIC_RAYCASTER_JSON_HELPERS_H
 
-#include "json.h"
+typedef struct JSONObject JSONObject;
+typedef struct JSONValue JSONValue;
+typedef struct JSONElement JSONElement;
+typedef struct JSONArray JSONArray;
 
 void print_JSONValue(JSONValue *JSONValueRef);
 void print_JSONElement(JSONElement *JSONElementRef);
--- source/json/json_parsers.h
+++ source/json/json_parsers.h
@@ -5,6 +5,15 @@
 #ifndef CS430_PROJECT_2_BASIC_RAYCASTER_PARSERS_H
 #define CS430_PROJECT_2_BASIC_RAYCASTER_PARSERS_H
 
+typedef struct JSONObject JSONObject;
+typedef struct JSONValue JSONValue;
+typedef struct JSONElement JSONElement;
+typedef struct JSONArray JSONArray;
+
 char* parse_string(FILE *fh);
+int read_JSONValue(FILE *fp, JSONValue *JSONValueRef);
+int read_JSONObject(FILE *fp, JSONObject *JSONObjectRef);
+int read_JSONElement(FILE *fp, JSONElement *JSONElementRef);
+int read_JSONArray(FILE *fp, JSONArray *JSONArrayRef);
 
 #endif //CS430_PROJECT_2_BASIC_RAYCASTER_PARSERS_H
